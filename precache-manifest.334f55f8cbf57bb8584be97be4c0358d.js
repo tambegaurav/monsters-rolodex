764e3ac9dd3e7a35c222b5609b6bd9c72c70ee36
tambegaurav <gauravkishortambe@gmail.com>
2020-07-30 01:19:43 +0530

    Updates

---
+++ precache-manifest.334f55f8cbf57bb8584be97be4c0358d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6494d5e9c0848db31769caefd932b05",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c666cd38cc96dba456d2",
+    "url": "/monsters-rolodex/static/css/main.8618fe32.chunk.css"
+  },
+  {
+    "revision": "8a0a6a3b86071050ef43",
+    "url": "/monsters-rolodex/static/js/2.9f41759d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9f41759d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c666cd38cc96dba456d2",
+    "url": "/monsters-rolodex/static/js/main.ca319f85.chunk.js"
+  },
+  {
+    "revision": "e1c6aa9c969cda92eead",
+    "url": "/monsters-rolodex/static/js/runtime-main.19e38e03.js"
+  }
+]);
